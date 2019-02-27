6d8d576ec1a0af610b2860588cae54a7495a789f
mattia richetto <mattia.richetto@gmail.com>
2019-02-26 21:25:40 -0800

    Updates

---
+++ precache-manifest.bf4e14fe3237514ea2848e9f793af4ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cba018557911830fa5c7",
+    "url": "/pace-converter-app/static/js/runtime~main.cba01855.js"
+  },
+  {
+    "revision": "1f4378b06331ea72469b",
+    "url": "/pace-converter-app/static/js/main.1f4378b0.chunk.js"
+  },
+  {
+    "revision": "a14d8c4a0cb4b6d36e52",
+    "url": "/pace-converter-app/static/js/2.a14d8c4a.chunk.js"
+  },
+  {
+    "revision": "1f4378b06331ea72469b",
+    "url": "/pace-converter-app/static/css/main.30dd9f65.chunk.css"
+  },
+  {
+    "revision": "5afd541f2bd092cc109aca28132ee09d",
+    "url": "/pace-converter-app/index.html"
+  }
+];
